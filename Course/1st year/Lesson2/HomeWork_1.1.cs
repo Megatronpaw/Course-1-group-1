b3fa3de1b8d8152254f2dfdcb22fdf0d72ae06f0
iliykalaw@gmail <iliykalaw@gmail.com>
2024-11-23 22:26:18 +0900

    Домашняя работа_1

---
+++ Course/1st year/Lesson2/HomeWork_1.1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число.");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе число.");
+            int b = int.Parse(Console.ReadLine());
+            int ans = a + b;
+            Console.WriteLine(ans);
+        }
+    }
+}
---
+++ Course/1st year/Lesson2/HomeWork_1.2.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_1._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool ver = true;
+            bool ver2 = true;
+            int a = 52;
+            Console.WriteLine("Я загадал число. Попробуй отгадать за 3 попытки. Подсказка: число двухзначное:");
+            int b = int.Parse(Console.ReadLine());
+            if (b < a)
+            {
+                Console.WriteLine("Неверно! Больше");
+            }
+            else if (b > a)
+            {
+                Console.WriteLine("Неверно! Меньше");
+
+            }
+            else
+            {
+                Console.WriteLine("Верно! Молодец!");
+                ver = false;
+            }
+
+            if (ver)
+            {
+                Console.WriteLine("Попробуйте ещё раз! Осталось 2 попытки");
+                b = int.Parse(Console.ReadLine());
+                if (b < a)
+                {
+                    Console.WriteLine("Неверно! Больше");
+                    ver2 = false;
+                }
+                else if (b > a)
+                {
+                    Console.WriteLine("Неверно! Меньше");
+                    ver2 = false;
+                }
+                else
+                {
+                    Console.WriteLine("Верно! Молодец!");
+                    
+                }
+            }
+            if (!ver2)
+            {
+                Console.WriteLine("Попробуйте ещё раз! Осталось 1 попытка");
+                b = int.Parse(Console.ReadLine());
+                if (b < a)
+                {
+                    Console.WriteLine("Неверно! Вы проиграли. Ответ 52.");
+                }
+                else if (b > a)
+                {
+                    Console.WriteLine("Неверно! Вы проиграли. Ответ 52.");
+
+                }
+                else
+                {
+                    Console.WriteLine("Верно! Молодец!");
+                }
+            }
+        }
+
+    }
+}
