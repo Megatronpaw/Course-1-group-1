de3d4ec61e6adc3e63044f54cc3d39b2a85da8b3
iliykalaw@gmail <iliykalaw@gmail.com>
2024-11-23 19:25:19 +0900

    Урок 2 практика abc

---
+++ Course/1st year/Lesson2/Proverok_1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend_Lessen1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Практика А. 1)
+             Console.WriteLine("Введите имя...");
+             string name = Console.ReadLine();
+             Console.WriteLine("Добро пожаловать, " + name + ", в мою программу!"); 
+            
+            2)
+            Console.WriteLine("Введите свой год рождения");
+            int year = int.Parse(Console.ReadLine());
+            int age = 2024 - year;
+            Console.WriteLine("Ваш возраст:" + age);
+            
+            Практика Б. 1)
+
+            Console.WriteLine("Новый контакт. Введите имя контакта");
+            string name = Console.ReadLine();
+            Console.WriteLine("Введите номер контакта");
+            string number = Console.ReadLine();
+            Console.WriteLine("Имя: " + name);
+            Console.WriteLine("Номер контакта: " + number);
+
+            2)
+
+            Console.WriteLine("Введите 2 числа");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            if (a > b)
+            {
+                Console.WriteLine(a);
+                Console.WriteLine(b);
+            }
+            else
+            {
+                Console.WriteLine(b);
+                Console.WriteLine(a);
+            }
+
+            Практика С. 1)
+            
+            Console.WriteLine("Введите число.");
+            int a = int.Parse(Console.ReadLine());
+            int sot = (a / 100);
+            int des = (a / 10) % 10;
+            int ed = (a / 1) % 10;
+            Console.WriteLine("Количество сотен: " + sot);
+            Console.WriteLine("Количество десятки: " + des);
+            Console.WriteLine("Количество едениц: " + ed);
+            */
+        }
+    }
+}
