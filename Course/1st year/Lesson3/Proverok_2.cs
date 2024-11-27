f847d72fe96ff73fce42d0f433aa4369b3d1c388
iliykalaw@gmail <iliykalaw@gmail.com>
2024-11-27 18:26:44 +0900

    Проверочная_2

---
+++ Course/1st year/Lesson3/Proverok_2.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proverok_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Практика А. 1)
+             
+            int[] num = new int[5];
+            2) 
+            string[] str = new string[3];
+
+            Практика Б. 3)
+
+            int[] num = { 1, 2, 3 };
+            Console.WriteLine(num[1]);
+            4)
+            int[] num = { 1, 2, 3 };
+            num[2] = 100;
+            5)
+            int[] num = { 1, 2, 3 };
+            Console.WriteLine(num.Length);
+
+            Практика С. 6)
+
+            int[] num = new int[4];
+            7)
+            int[] num = new int[5];
+            8)
+            int[] num = {1, 2, 3};
+            9)
+            int[] num = new int[5];
+            num[0] = 10;
+            num[1] = 21;
+            num[2] = 32;
+            num[3] = 43;
+            num[4] = 54;
+            */
+        }
+    }
+}
