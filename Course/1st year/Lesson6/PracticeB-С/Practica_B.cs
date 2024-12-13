3efec962bbce00e99e200c34d3963fade49ce54c
iliykalaw@gmail <iliykalaw@gmail.com>
2024-12-13 18:29:23 +0900

    Практика Б

---
+++ Course/1st year/Lesson6/PracticeB-С/Practica_B.cs
@@ -0,0 +1,44 @@
+﻿int[] num = { -3, 4, 9, -5, 8, 1, 5, 3, -7 };
+int N = 0;
+Console.WriteLine("Введите величину подмассива");
+int Enter = int.Parse(Console.ReadLine());
+if (Enter < num.Length)
+{
+    N = Enter;
+}
+else
+{
+    N = num.Length;
+}
+int maxSum = -52;
+int[] MaxSumArray(int[] array)
+{
+    int[] num2 = new int[N];
+    int[] result = new int[N];
+    int a = 0;
+    for (int i = 0; i <= num.Length - N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            a += num[i + j];
+            num2[j] = num[i + j];
+
+        }
+        if (maxSum < a)
+        {
+            maxSum = a;
+            for (int k = 0; k < N; k++)
+            {
+                result[k] = num2[k];
+            }
+        }
+        a = 0;
+    }
+    return result;
+}
+Console.Write("Подмассив: ");
+foreach (int i in MaxSumArray(num))
+{
+    Console.Write(i + " ");
+}
+Console.WriteLine($"\nСумма равна: {maxSum}");
