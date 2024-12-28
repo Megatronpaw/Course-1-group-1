4cee0c641a5caeb4bed4385d68a241c32d475b69
iliykalaw@gmail <iliykalaw@gmail.com>
2024-12-28 23:10:43 +0900

    Проверочная C

---
+++ Course/1st year/Lesson6/PracticeB-С/Practica_C.cs
@@ -0,0 +1,25 @@
+﻿int[] num = { 1, 3, 5};
+static void ChangeNum(int[] array, int a)
+{
+    if (a == array.Length - 1)
+    {
+        
+        foreach (int i in array)
+        {
+            Console.Write(i+ " ");
+        }
+        Console.WriteLine("\n");
+        return;
+    }
+    for (int i = a; i < array.Length; i++)
+    {
+        int obm = array[a];
+        array[a] = array[i];
+        array[i] = obm;
+        ChangeNum(array, a + 1);
+        obm = array[a];
+        array[a] = array[i];
+        array[i] = obm;
+    }
+}
+ChangeNum(num, 0);
