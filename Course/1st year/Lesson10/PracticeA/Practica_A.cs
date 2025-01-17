7f15ac52baa66533c844a7aae6a26f3ef7044d60
iliykalaw@gmail <iliykalaw@gmail.com>
2025-01-17 15:05:40 +0900

    Практика урок 10 А

---
+++ Course/1st year/Lesson10/PracticeA/Practica_A.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+namespace testtt
+{
+    internal class Program
+    {
+            static void WriteFile()
+            {
+                string[] hello = new string[5];
+                for (int i = 0; i < hello.Length; i++)
+                {
+                    hello[i] = "Hello, World!";
+                }
+                File.WriteAllLines("test.txt", hello);
+            }
+            static void ReadFile()
+            {
+                string[] hello = File.ReadAllLines("test.txt");
+                foreach (string line in hello)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            static void Main(string[] arg)
+            {
+                WriteFile();
+                ReadFile();
+            }
+     }
+ }
